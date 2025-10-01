ff95f38bb8144696df150fdc18ae8d50f54e1391
necoian <0108.facultad@gmail.com>
2025-10-01 19:23:25 -0300

    En Prueba, se agrega el Actualizar

--- src/Control/prueba.java
+++ src/Control/prueba.java
@@ -14,27 +14,32 @@ public class prueba {
   Alumno a3= new Alumno(4102934,"Ramiro","Moran",LocalDate.of(2000, 3, 9),true);
   Alumno a4= new Alumno(4032122,"Juan","Villegas",LocalDate.of(1997, 12, 03),true);
   Alumno a5= new Alumno(0000001,"Prueba","Prueba",LocalDate.of(1992, 05, 03),true);
+  Alumno a6= new Alumno(0000002,"cambiar","cambiar",LocalDate.of(1820, 12, 10),true);
   //Creamos la conexion.
   Conexion conexion = new Conexion("jdbc:mariadb://localhost/gp18universidad","root","");
   //creamos alumnosdata junto con la conexion a la bd
   AlumnosData alumnos = new AlumnosData(conexion);
-  /*
-  alumnos.cargarAlumnos(INSERT)................................
+  
+  //alumnos.cargarAlumnos(INSERT)................................
   alumnos.cargarAlumno(a1);
   alumnos.cargarAlumno(a2);
   alumnos.cargarAlumno(a3);
   alumnos.cargarAlumno(a4);
   alumnos.cargarAlumno(a5);
+  alumnos.cargarAlumno(a6);
   
-  
-  alumno.darDeBaja alumno.darDeAlta(UPDATE)................................
+  //alumno.darDeBaja alumno.darDeAlta(UPDATE)................................
   alumnos.darDeBaja(0000001);
   alumnos.darDeAlta(0000001);
   
-  alumno.borrarAlumno(DELETE)................................
+  //alumno.borrarAlumno(DELETE)................................
   alumnos.borrarAlumno(0000001);
   
-  */
+  
+  //alumnos.actualizarAlumno() (ACTUALIZAR)
+  a6.setApellido("Ricardo");
+  alumnos.actualizarAlumno(a6);
+  
   //alumno.buscar(SELECT)................................
   alumnos.buscarAlumno(2);
   //metodo que retorna una lista de todos los alumnos(SELECT *)................................
